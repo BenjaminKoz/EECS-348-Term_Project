2892138e95ea40bd0706acc687b8fc0433ff9c30
MJ-McGee <143756369+MJ-McGee@users.noreply.github.com>
2023-11-17 14:30:04 -0600

    Add files via upload

---
+++ evaluator.cpp
@@ -0,0 +1,34 @@
+/*
+Implementation of the evaluator function for the Arithmetic Expression Calculator software
+
+*/
+#include <math.h>
+#include <iostream>
+
+
+//Function to evaluate 2 numbers with a given operator.
+double eval(int x, int y, char oper) {
+    switch(oper) {
+        case 37: //% sign operator
+            return x % y;
+        case 42: //* operator
+            return x * y;        
+        case 43: //+ operator
+            return x + y;
+        case 45: //- operator
+            return x - y;
+        case 47: //'/' operator
+            return x / y;
+        case 94: //^ operator
+            return pow(x,y);
+        default: //If the given operator is not recognized.
+            throw "Invalid operator";
+    }
+}
+
+/*
+int main() {
+    double ans = eval(2,2,'%');
+    std::cout << ans;
+}
+*/
