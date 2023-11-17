8d4fcb7e14555253fc9435a293f6808aedf8922b
qgan99 <37915115+qgan99@users.noreply.github.com>
2023-11-17 15:16:32 -0600

    Update main.cpp to catch grouping symbols error

--- main.cpp
+++ main.cpp
@@ -9,6 +9,20 @@ int main(){
     std::cout << "Input your expression to be evaluated: ";
     std::cin >> inputString;
     try {
+        int closed = 0;
+        for (int i = 0; i < inputString.length(); i++){
+            if (inputString[i] == '('){
+                closed++;
+            }else if(inputString[i] == ')'){
+                closed--;
+                if (closed < 0){
+                    throw "Invalid Grouping Symbols";
+                }
+            }
+        }
+        if(closed != 0){
+            throw "Invalid Grouping Symbols";
+        }
         double returnThis = parser(inputString);
         cout << returnThis;
     }
